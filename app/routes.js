e785dcc387db10493801f9ea96d826d3bf665eb8
Guilherme Almeida <gui.costa123@hotmail.com>
2018-01-04 19:31:48 -0200

    route

--- app/routes.js
+++ app/routes.js
@@ -1,10 +1,11 @@
 app.config(['$stateProvider', '$urlRouterProvider', '$locationProvider',
 
   function ($stateProvider, $urlRouterProvider, $locationProvider) {
-    $locationProvider.html5Mode({
-      enabled: true,
-      requireBase: false
-    });
+    // $locationProvider.html5Mode({
+    //   enabled: true,
+    //   requireBase: false
+    // });
+    $locationProvider.html5Mode(false).hashPrefix('');
     $urlRouterProvider.otherwise('/');
     $stateProvider
 
